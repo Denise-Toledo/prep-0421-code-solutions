19265734e5381d52ec29571ca7fd082466a10de7
Denise Toledo <denisetoledo.91@gmail.com>
2021-05-01 01:31:23 -0700

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var a = 0; a < 20; a += 2) {
+  console.log(a);
+}
+
+for (var b = 100; b > 0; b--) {
+  console.log('Time till explosion:', b);
+}
